42320ed6b4226250b7ad19d10dc3385836fd6739
amormz <amormz@foxmail.com>
2021-06-23 21:14:32 +0800

    添加Swagger3

--- src/main/java/pers/muzi/bbs/BbsApplication.java
+++ src/main/java/pers/muzi/bbs/BbsApplication.java
@@ -1,9 +1,16 @@
 package pers.muzi.bbs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+/**
+ * @author AmorMz
+ */
 @SpringBootApplication
+@MapperScan({"pers.muzi.bbs.dao"})
+@EnableOpenApi
 public class BbsApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pers/muzi/bbs/config/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package pers.muzi.bbs.config.swagger;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author AmorMz
+ * Swagger配置类
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("木子BBS接口文档")
+                .description("github开源地址请点击下方Website")
+                .contact(new Contact("AmorMz", "https://github.com/AmorMz/bbs-server-v2.0", "amormz@foxmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
