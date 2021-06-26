afd8598c2c1ee07b2cc8412f8e46cda282d066fd
amormz <amormz@foxmail.com>
2021-06-26 22:02:08 +0800

    优化登录逻辑

--- src/main/java/pers/muzi/bbs/controller/UserController.java
+++ src/main/java/pers/muzi/bbs/controller/UserController.java
@@ -1,7 +1,39 @@
 package pers.muzi.bbs.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pers.muzi.bbs.annotation.LoginRequired;
+import pers.muzi.bbs.common.result.Resp;
+import pers.muzi.bbs.entity.User;
+import pers.muzi.bbs.entity.vo.user.UserInfoVO;
+import pers.muzi.bbs.interceptor.AuthInterceptor;
+import pers.muzi.bbs.service.UserService;
+
 /**
  * @author AmorMz
  */
+@RestController
+@RequestMapping("/user")
+@Api(tags = "用户")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation("获取当前登录用户信息")
+    @GetMapping("/info")
+    @LoginRequired
+    public Resp getUserInfo() {
+        // 当前登录用户id
+        Integer id = AuthInterceptor.getId();
+        User user = userService.getUserById(id);
+        UserInfoVO userInfoVO = new UserInfoVO();
+        BeanUtils.copyProperties(user, userInfoVO);
+        return Resp.ok().data("userInfo", userInfoVO);
+    }
 }
